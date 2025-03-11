41588c653cecbf363fe362a7613c485f1330aedc
박형석 <baghyeongseog@baghyeongseog-ui-MacBookAir.local>
2025-03-11 14:03:32 +0900

    image size fix

--- app/(tabs)/croquis/page.tsx
+++ app/(tabs)/croquis/page.tsx
@@ -6,16 +6,16 @@ export default function Croquis() {
     <div className="relative">
       <div className="relative">
         <Image
-          width={600}
-          height={100}
+          width={1980}
+          height={1024}
           src="/images/backphoto.png"
           alt="background"
-          className="w-full h-[400px] object-cover rounded-lg"
+          className="w-full max-h-[400px] object-cover rounded-lg"
           quality={100}
           priority
         />
         <span className="absolute top-1/2 left-8 transform -translate-y-1/2 text-white text-lg font-medium max-w-xl text-shadow">
-          <span className="text-3xl font-bold">Unsplash </span>
+          <span className="text-2xl font-bold">Unsplash </span>
           에서 제공하는 다양한 이미지를 활용해,
           <br />
           집중력을 높일 수 있는 타이머와 함께 크로키를 즐길 수 있는
